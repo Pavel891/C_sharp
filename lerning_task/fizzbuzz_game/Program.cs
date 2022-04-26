81f8f1e38c653d938d463aa6b234d81c0ef0102e
Pavel G <pavelg0323@live.com>
2022-04-26 14:04:01 +0500

    Initial commit

---
+++ lerning_task/fizzbuzz_game/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int x = 100;
+
+		for (int i = 1; i <= x; i++)
+		{
+			if ((i % 3 == 0) && (i % 5 == 0))
+			{
+				Write(" FizzBuzz ");
+			}
+			else if (i % 5 == 0)
+			{
+				Write(" Buzz ");
+			}
+			else if (i % 3 == 0)
+			{
+				Write(" Fizz ");
+			}
+			else
+			{
+				Write($" {i} ");
+			}
+		}
+	}
+}
