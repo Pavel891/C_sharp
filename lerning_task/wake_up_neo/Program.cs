234841a323d9c83c739fd73a2d1d7b9c7fc5cab1
Pavel G <pavelg0323@live.com>
2022-04-27 21:44:24 +0500

    Upload new lerning program

---
+++ lerning_task/wake_up_neo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Text;
+using System.Linq;
+using System.Diagnostics;
+
+namespace wake_up_neo
+{
+	class Program
+	{
+		static void Write_frace(string words)
+		{
+			foreach (char i in words)
+			{
+				Thread.Sleep(90);
+				Write(i);
+			}
+			Thread.Sleep(4000);
+			Console.Clear();
+		}
+		static void Main(string[] args)
+		{
+			ForegroundColor = ConsoleColor.Green;
+			string s1 = "Wake up Neo...";
+			string s2 = "Matrix has you...";
+			string s3 = "Follow the white rabbit...";
+
+			Write_frace(s1);
+			Write_frace(s2);
+			Write_frace(s3);
+		}
+	}
+}
